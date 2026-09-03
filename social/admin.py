6916c6c52f066eb2fbca7ea7c7ce683337f6286e
Mumtahina Naaeem <maanmumtahina@gmail.com>
2026-09-03 13:25:50 +0500

    Add all social user fields to admin

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "full_name",
+        "bio",
+        "profile_picture",
+        "cover_photo",
+        "location",
+        "created_at",
+        "updated_at",
+    )
+
+    search_fields = (
+        "id",
+        "username",
+        "email",
+        "full_name",
+        "location",
+    )
+
+    readonly_fields = (
+        "id",
+        "created_at",
+        "updated_at",
+    )
+
+    filter_horizontal = (
+        "followers",
+        "following",
+        "connections",
+    )
