fea95bbf333c2d0ed159e61b9004e0a513357c8d
Mumtahina Naaeem <maanmumtahina@gmail.com>
2026-09-08 11:50:11 +0500

    Fix Django settings for ASGI

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,18 +1,16 @@
 
 import os
 
-from django.core.asgi import get_asgi_application
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-from social.websocket_auth import ClerkWebSocketAuthMiddleware
-from social.routing import websocket_urlpatterns
-
-
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "backend.settings",
 )
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from social.websocket_auth import ClerkWebSocketAuthMiddleware
+from social.routing import websocket_urlpatterns
 
 
 django_asgi_app = get_asgi_application()
@@ -23,9 +21,7 @@ application = ProtocolTypeRouter(
         "http": django_asgi_app,
 
         "websocket": ClerkWebSocketAuthMiddleware(
-            URLRouter(
-                websocket_urlpatterns
-            )
+            URLRouter(websocket_urlpatterns)
         ),
     }
 )
