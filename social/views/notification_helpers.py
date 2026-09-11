2985fa3a7ceeccefd46ff7cea1842aa054ddd356
Mumtahina Naaeem <maanmumtahina@gmail.com>
2026-09-11 08:03:40 +0500

    Fix real-time notification count and sound

--- social/views/notification_helpers.py
+++ social/views/notification_helpers.py
@@ -1,3 +1,6 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
 from ..models import Notification
 
 
@@ -11,10 +14,30 @@ def create_notification(
     if recipient.id == actor.id:
         return
 
-    Notification.objects.create(
+    notification = Notification.objects.create(
         recipient=recipient,
         actor=actor,
         notification_type=notification_type,
         message=message,
         post=post,
+    )
+
+    channel_layer = get_channel_layer()
+
+    async_to_sync(channel_layer.group_send)(
+        f"notifications_{recipient.id}",
+        {
+            "type": "notification_message",
+            "notification": {
+                "id": notification.id,
+                "notification_type": notification.notification_type,
+                "message": notification.message,
+                "post_id": post.id if post else None,
+                "actor": {
+                    "id": actor.id,
+                    "username": actor.username,
+                    "full_name": actor.full_name,
+                },
+            },
+        },
     )
